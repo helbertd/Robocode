77155f6711c0c8bb223ceb4b898d0ecff6c2c604
Helbertd <helbertdt@gmail.com>
2024-10-03 22:49:05 -0300

    Primeiro Codigo

---
+++ FURY.java
@@ -0,0 +1,46 @@
+package HelbertDias;
+import robocode.*;
+
+
+/**
+ * FURY - a robot by (Helbert Dias Torres)
+ */
+public class FURY extends Robot
+{
+	/**
+	 * run: FURY's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(150);
+			turnGunRight(360);
+			turnRight(180);
+			ahead(150);
+			turnGunRight(360);
+		}
+	}
+		public void onScannedRobot(ScannedRobotEvent e) {
+		fire(2);
+	}
+
+		public void onHitByBullet(HitByBulletEvent e) {
+		back(100);
+		turnLeft(110);
+		
+	}
+		public void onHitWall(HitWallEvent e) {
+		back(50);
+		turnRight(100);
+		ahead(100);
+	}
+
+}
