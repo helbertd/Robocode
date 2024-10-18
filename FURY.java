6bc719416952994dd62aa7afea7a6eb94e9b9b2a
helbertd <helbertdt@gmail.com>
2024-10-18 00:14:33 -0300

    Update FURY.java

--- FURY.java
+++ FURY.java
@@ -1,50 +1,51 @@
 package HelbertDias;
 import robocode.*;
-
+import static robocode.util.Utils.*;
+import java.awt.Color;
 
 /**
  * FURY - a robot by (Helbert Dias Torres)
  */
 public class FURY extends Robot
 {
-	/**
-	 * run: FURY's default behavior
-	 */
-	public void run() {
-		// Initialization of the robot should be put here
+    public void run() {  
+        setColors(Color.RED, Color.BLUE, Color.YELLOW); 
+        while (true) {
+            ahead(200);
+            turnGunRight(360);
+            scan();
+            turnRight(90);
+        }
+    }
 
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
+    public void onScannedRobot(ScannedRobotEvent e) {
+         
+            fire(2);
+        
+    }
 
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+    public void onHitByBullet(HitByBulletEvent e) {
+        scan();
+		turnGunRight(360);
+		ahead(200);
+        turnLeft(90);
+        
+    }
 
-		// Robot main loop
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(150);
-			turnGunRight(360);
-			turnRight(180);
-			ahead(150);
-			turnGunRight(360);
-		}
-	}
-		public void onScannedRobot(ScannedRobotEvent e) {
-		fire(2);
-	}
+    public void onHitWall(HitWallEvent e) {
+        back(30);
+        turnRight(90);
+        ahead(150);
+    }
 
-		public void onHitByBullet(HitByBulletEvent e) {
-		ahead(100);
-		turnLeft(90);
-		
-	}
-		public void onHitWall(HitWallEvent e) {
-		back(50);
-		turnRight(100);
-		ahead(100);
-	}
-		public void onHitRobot(HitRobotEvent inimigo) {
-		turnRight(inimigo.getBearing());
-		fire(3);
+    public void onHitRobot(HitRobotEvent e) {
+        double bearing = e.getBearing();
+        double heading = getHeading();
+        double gunHeading = getGunHeading();
 
-	}
+        double turnGunAmt = normalRelativeAngleDegrees(bearing + heading - gunHeading);
+        turnGunRight(turnGunAmt);
+	fire(3);
+        
+    }
 }
